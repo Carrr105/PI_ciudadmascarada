3c46ad83cffaf9ddf6100cabd0a742921daf01dc
Miguel <a00822952@itesm.mx>
2022-04-04 18:00:10 -0500

    Editor de texto y otros

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Edit from './pages/Edit'
+import Main from './pages/Main'
+import New from './pages/New'
+import View from './pages/View'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/view' element={<View />} />
+        <Route path='/edit' element={<Edit />} />
+        <Route path='/new' element={<New />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ my-app/src/pages/View.js
@@ -0,0 +1,9 @@
+function View() {
+    return (
+        <>
+            <h1>Ver Titulo</h1>
+        </>
+    )
+}
+
+export default View
